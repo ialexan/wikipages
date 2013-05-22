15a753af39a11700c32d1e339f84dde42fa05cb1
ishag alexanian <ishagalexanian@gmail.com>
2013-05-22 09:04:53 -0700

    Added deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: Ishag Alexanian
+# Deploying the war 
+
+TOMCAT=/home/ec2-user/apache-tomcat-7.0.40/webapps
+
+rm -rf $TOMCAT/wikipages
+mkdir $TOMCAT/wikipages
+
+cp WebContent/target/wikipages.war $TOMCAT/wikipages/
+
